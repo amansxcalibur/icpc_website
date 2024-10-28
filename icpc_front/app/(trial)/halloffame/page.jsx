e30a6ae786e1c0e6fcd577b80a743cc6550f62b5
amansxcalibur <notifyamanvs@gmail.com>
2024-10-29 04:19:25 +0530

    fixed overflow in perks

--- icpc_front/app/(trial)/halloffame/page.jsx
+++ icpc_front/app/(trial)/halloffame/page.jsx
@@ -3,7 +3,9 @@ import Terminal from "@/components/ui_elems/terminal/terminal_2";
 
 export default function page2(){
     return(
-        // <HallOfFame/>
-        <Terminal/>
+        <div className="pt-[9vw]">
+        <HallOfFame/>
+        {/* <Terminal/> */}
+        </div>
     )
 }
