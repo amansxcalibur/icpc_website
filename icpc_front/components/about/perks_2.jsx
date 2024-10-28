1f96b54ee76cc14e43d26c47c016051425102b60
amansxcalibur <notifyamanvs@gmail.com>
2024-10-29 04:00:37 +0530

    added available links and images

--- icpc_front/components/about/perks_2.jsx
+++ icpc_front/components/about/perks_2.jsx
@@ -42,7 +42,7 @@ export default function ExclusivePerks2() {
             <div className="flex justify-center min-w-[80vw] mt-[2vw] flex-col">
                 <div className="flex-1 min-h-[10vw] flex items-center mt-[5vw] max-md:flex-col">
                     <div className="flex-1 flex justify-center">
-                        <div className="min-h-[20vw] max-md:min-h-[60vw] max-md:min-w-[90vw] min-w-[30vw] bg-gray-500 rounded-[2vw]"></div>
+                        <Image src="/more_people_icpc.jpg" width={1} height={1} unoptimized className="min-h-[20vw] w-auto max-md:min-h-[60vw] max-md:min-w-[90vw] min-w-[30vw] bg-gray-500 rounded-[2vw]"></Image>
                     </div>
                     <div className="flex-1 flex flex-col items-center">
                         <p className="text-[1.5vw] text-center font-semibold max-md:text-[4vw]">Expanded On-Site Participation</p>
@@ -55,12 +55,12 @@ export default function ExclusivePerks2() {
                         <p className="text-[1.5vw] text-center max-w-[30vw] max-md:max-w-[90vw] max-md:text-[4vw]">Participants will gain access to an exclusive webinar led by an ICPC World Finalist, offering invaluable tips and strategies to enhance their ICPC performance.</p>
                     </div>
                     <div className="flex-1 flex justify-center">
-                        <div className="min-h-[20vw] max-md:min-h-[60vw] max-md:min-w-[90vw] min-w-[30vw] bg-gray-500 rounded-[2vw]"></div>
+                        <Image src="/webinar.jpg" width={1} height={1} unoptimized className="min-h-[20vw] max-md:min-h-[60vw] max-md:min-w-[90vw] min-w-[30vw] bg-gray-500 rounded-[2vw] w-auto"></Image>
                     </div>
                 </div>
                 <div className="flex-1 min-h-[10vw] flex items-center mt-[10vw] max-md:flex-col">
                     <div className="flex-1 flex justify-center">
-                        <div className="min-h-[20vw] min-w-[30vw] max-md:min-h-[60vw] max-md:min-w-[90vw] bg-gray-500 rounded-[2vw]"></div>
+                        <Image src="/winner_icpc.jpg" width={1} height={1} unoptimized className="min-h-[20vw] w-auto min-w-[30vw] max-md:min-h-[60vw] max-md:min-w-[90vw] bg-gray-500 rounded-[2vw]"></Image>
                     </div>
                     <div className="flex-1 flex flex-col items-center">
                         <p className="text-[1.5vw] text-center font-semibold max-md:text-[4vw]">Noteworthy Certificates</p>
@@ -73,7 +73,7 @@ export default function ExclusivePerks2() {
                         <p className="text-[1.5vw] text-center max-w-[30vw] max-md:max-w-[90vw] max-md:text-[4vw]">Coaches who get 5 or more teams registered get a special certificate in recognition for their efforts in fostering young talent.</p>
                     </div>
                     <div className="flex-1 flex justify-center">
-                        <div className="min-h-[20vw] min-w-[30vw] max-md:min-h-[60vw] max-md:min-w-[90vw] bg-gray-500 rounded-[2vw]"></div>
+                        <Image src="/smart_india.jpg" unoptimized width={1} height={1} className="min-h-[20vw] min-w-[30vw] max-md:min-h-[60vw] max-md:min-w-[90vw] bg-gray-500 rounded-[2vw]"></Image>
                     </div>
                 </div>
             </div>
--- icpc_front/components/about/why_participate.jsx
+++ icpc_front/components/about/why_participate.jsx
@@ -6,42 +6,44 @@ export default function WhyParticipate(){
             <div className="w-[70vw] max-md:w-[90vw] flex flex-col">
                 <div className="flex-1 flex max-md:flex-col">
                     <div className="flex-1 text-[3vw] tracking-tighter font-semibold max-md:text-[6vw]">
-                        Why Participate?
+                        Why Amritapuri?
                     </div>
                     <div className="flex-1 text-[1.3vw] max-md:text-[4vw]">
-                        Competitive programming is more than mastering algorithms—it's a comprehensive workout for your brain. When you dive into CP/DSA (Competitive Programming/Data Structures and Algorithms), 
-                        you don't just learn coding techniques; you cultivate a suite of invaluable skills that will serve you throughout your career and life.
+                        <p>Competitive programming is more than mastering algorithms—it's a comprehensive workout for your brain.</p>
+                        {/* When you dive into CP/DSA (Competitive Programming/Data Structures and Algorithms), 
+                        you don't just learn coding techniques; you cultivate a suite of invaluable skills that will serve you throughout your career and life. */}
+                        <p className="mt-[0.5vw]"><strong>Amritapuri</strong> isn't just a venue—it's an <span className="text-[#92133b]">experience</span>, you don't just learn coding techniques; you cultivate a suite of invaluable skills that will serve you throughout your career and life. Here's what you can expect at the regionals</p>
                     </div>
                 </div>
-                <div className="flex-1 flex mt-[4vw] max-md:flex-col">
+                <div className="flex-1 flex mt-[4vw] max-md:flex-col gap-x-[1vw]">
                     <div className="flex-1 flex flex-col justify-center items-center min-h-[25vw]">
                         <Image src="/bagpack.png" width={1} height={1} className="rounded-[2vw] h-[13vw] w-auto max-md:h-[40vw]" unoptimized></Image>
                         <div className="flex-1 mt-[1vw] text-[1.2vw] max-md:text-[4vw]">
-                            <p className="text-center font-semibold">Systems Thinking</p>
-                            <p className="text-center"> Understand complex systems by seeing how different components interact.</p>
+                            <p className="text-center font-semibold">Expert Organization</p>
+                            <p className="text-center">Benefit from our extensive experience in hosting ICPC regionals, ensuring a smooth and professional contest environmen</p>
                         </div>
                     </div>
                     <div className="flex-1 flex flex-col justify-center items-center min-h-[25vw]">
                         <Image src="/guy_learning.webp" width={1} height={1} className="rounded-[2vw] w-auto h-[13vw] max-md:h-[40vw]" unoptimized></Image>
                         <div className="flex-1 mt-[1vw] text-[1.2vw] max-md:text-[4vw]">
-                            <p className="text-center font-semibold">Rapid Learning</p>
-                            <p className="text-center">Accelerate your ability to absorb new concepts and technologies.</p>
+                            <p className="text-center font-semibold">State-of-the-Art Facilities</p>
+                            <p className="text-center">Access advanced air-conditioned labs and infrastructure designed to support peak performance.</p>
                         </div>
                     </div>
                     <div className="flex-1 flex flex-col justify-center items-center min-h-[25vw]">
                         <Image src="/guy_fix_server.webp" width={1} height={1} className="rounded-[2vw] w-auto h-[13vw] max-md:h-[40vw]" unoptimized></Image>
                         <div className="flex-1 mt-[1vw] text-[1.2vw] max-md:text-[4vw]">
-                            <p className="text-center font-semibold">Problem Deduction</p>
-                            <p className="text-center">Sharpen your skill in interpreting and dissecting problem statements to identify core challenges.</p>
+                            <p className="text-center font-semibold">Inspiring Atmosphere</p>
+                            <p className="text-center">Immerse yourself in an academic setting that promotes innovation, collaboration, and technical excellence.</p>
                         </div>
                     </div>
-                    <div className="flex-1 flex flex-col justify-center items-center min-h-[25vw]">
+                    {/* <div className="flex-1 flex flex-col justify-center items-center min-h-[25vw]">
                         <Image src="/trophy.webp" width={1} height={1} className="rounded-[2vw] w-auto h-[13vw] max-md:h-[40vw]" unoptimized></Image>
                         <div className="flex-1 mt-[1vw] text-[1.2vw] max-md:text-[4vw]">
                             <p className="text-center font-semibold">Algorithmic Thinking</p>
                             <p className="text-center">Enhance your capability to devise efficient, logical solutions to intricate problems.</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
