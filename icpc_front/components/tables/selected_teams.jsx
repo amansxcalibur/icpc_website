1610a9a0d6b7e7c8659482f21aaad56f6ad01cf8
amansxcalibur <notifyamanvs@gmail.com>
2024-12-03 22:01:09 +0530

    converted the selected provisional list to table

--- icpc_front/components/tables/selected_teams.jsx
+++ icpc_front/components/tables/selected_teams.jsx
@@ -1,21 +1,46 @@
 'use client';
 
-import React, { useState } from 'react';
-import selectedTeamsData from '../json/selected_teams.json';
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
 import MagnifyingGlass from '../svg/magnifying-glass';
+import DownlaodIcon from '../svg/download';
+import Link from 'next/link';
 
 export default function SelectedTeams() {
+    const [teamsData, setTeamsData] = useState([]);
     const [searchedVal, setSearchedVal] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
-    const teamsPerPage = 5;
+    const [showWomenOnly, setShowWomenOnly] = useState(false);
+    const teamsPerPage = 100;
 
-    // Filtered teams
-    const filteredTeams = selectedTeamsData.filter((row) =>
-        !searchedVal.length ||
-        row.teamName.toLowerCase().includes(searchedVal.toLowerCase()) ||
-        row.teamId.toString().toLowerCase().includes(searchedVal.toLowerCase()) ||
-        row.institute.toString().toLowerCase().includes(searchedVal.toLowerCase())
-    );
+    useEffect(() => {
+        const fetchCSVData = async () => {
+            const response = await fetch('/data/ICPC Selected Teams for Amritapuri Regionals 2024 Total team.csv');
+            const csvText = await response.text();
+            const parsedData = Papa.parse(csvText, {
+                header: true,
+                skipEmptyLines: true,
+            });
+            setTeamsData(parsedData.data.map(row => ({
+                ...row,
+                isWomenOnly: row.isWomenOnly == 'true',// for converting from string true to boolean true
+            })));
+        };
+
+        fetchCSVData();
+    }, []);
+
+    const filteredTeams = teamsData.filter((row) => {
+        const matchesSearch =
+            !searchedVal.length ||
+            row.teamName?.toLowerCase().includes(searchedVal.toLowerCase()) ||
+            row.teamId?.toString().toLowerCase().includes(searchedVal.toLowerCase()) ||
+            row.institution?.toLowerCase().includes(searchedVal.toLowerCase());
+
+        const matchesToggle = !showWomenOnly || row.isWomenOnly;
+
+        return matchesSearch && matchesToggle;
+    });
 
     // Pagination
     const indexOfLastTeam = currentPage * teamsPerPage;
@@ -35,40 +60,51 @@ export default function SelectedTeams() {
     return (
         <div className="min-h-screen flex flex-col items-center bg-[#92133b] pt-[8vw]">
             <div className="flex-1 flex flex-col min-w-[80vw] max-md:min-w-[95vw] max-md:max-w-[95vw]">
-                {/* Title Section */}
                 <div className="text-center mb-[4vw]">
                     <h1 className="text-[4vw] font-bold mb-[1vw] max-md:text-[8vw] text-white">
                         Selected Teams
                     </h1>
                     <p className="text-[1.2vw] max-md:text-[3.5vw] text-gray-200">
-                        Congratulations to all who have been selected to the ICPC Amritapuri Regionals
+                        Congratulations to all who have been selected to the ICPC Amritapuri Regionals.
                     </p>
                 </div>
-                {/* Search */}
-                <div className="flex justify-center">
-                    <div className="flex rounded-full min-h-[3vw] min-w-[30vw] mb-[2vw] px-[.5vw] gap-[.5vw] overflow-hidden bg-white items-center">
+                <div className="flex justify-center items-center mb-[2vw]">
+                    {/* Search Input */}
+                    <div className="flex rounded-l-full min-h-[3vw] min-w-[25vw] px-[.5vw] gap-[.5vw] overflow-hidden bg-white items-center">
                         <p className="text-stone-400 size-[2vw]">
                             <MagnifyingGlass />
                         </p>
                         <input
                             type="text"
-                            className="text-[1.1vw] max-md:text-[3vw] text-black flex-1 h-full"
+                            className="text-[1.1vw] max-md:text-[3vw] text-black flex-1 min-h-[3vw] outline-none"
                             placeholder="Search ID or team name"
                             onChange={(e) => setSearchedVal(e.target.value)}
                         />
                     </div>
+                    <div className="relative bg-white rounded-r-full">
+                        <select
+                            onChange={(e) => setShowWomenOnly(e.target.value === 'womenOnly')}
+                            value={showWomenOnly ? 'womenOnly' : 'allTeams'}
+                            className="px-[1vw] rounded-full py-[.5vw] min-h-[3vw] bg-gray-800 border border-solid border-gray-800 text-white cursor-pointer"
+                        >
+                            <option value="allTeams" className="bg-gray-800 text-white">
+                                All Teams
+                            </option>
+                            <option value="womenOnly" className="bg-gray-800 text-white">
+                                Women Only
+                            </option>
+                        </select>
+                    </div>
+                    <Link href="selected-teams-v1" title='Download the pdf containing selected teams' className='mx-[.5vw] p-[.5vw] hover:bg-gray-800 rounded-full min-h-[3vw] min-w-[3vw] flex justify-center items-center'><DownlaodIcon/></Link>
                 </div>
 
-                {/* Table Header */}
+                {/* Table */}
                 <div className="flex min-h-[4vw] items-center text-[1.2vw] max-md:text-[3vw] font-semibold text-white border-b border-white/30 pb-[1vw]">
                     <div className="flex-1">Team ID</div>
                     <div className="flex-[2]">Team Name</div>
-                    <div className="flex-[2]">Members</div>
-                    <div className="flex-[2]">College</div>
-                    <div className="flex-1">Score</div>
+                    <div className="flex-[2]">Institution</div>
                 </div>
 
-                {/* Table Content */}
                 <div className="mt-[2vw] space-y-[2vw]">
                     {currentTeams.map((entry, index) => (
                         <div
@@ -82,21 +118,12 @@ export default function SelectedTeams() {
                                 <span className="font-semibold text-white">{entry.teamName}</span>
                             </div>
                             <div className="flex-[2] text-[1.1vw] max-md:text-[3vw] text-gray-200">
-                                {entry.members.map((member, idx) => (
-                                    <span key={idx}>{member}<br /></span>
-                                ))}
-                            </div>
-                            <div className="flex-[2] text-[1.1vw] max-md:text-[3vw] text-gray-200">
-                                {entry.institute}
-                            </div>
-                            <div className="flex-1 text-[1.1vw] max-md:text-[3vw] text-gray-300">
-                                {entry.score}
+                                {entry.institution}
                             </div>
                         </div>
                     ))}
                 </div>
 
-                {/* Pagination */}
                 <div className="flex justify-center items-center my-[2vw] text-white gap-4">
                     <button
                         className={`px-[1vw] py-[.5vw] rounded-[.2vw] ${currentPage === 1 ? 'bg-gray-500 cursor-not-allowed' : 'bg-gray-800 hover:bg-gray-700'}`}
