ac071a5f7985872259c71c9a6644ba00e3ad82eb
AswinJ1 <aswinjhome@gmail.com>
2025-08-05 12:19:18 +0530

    refactor: replaced the google analytics tags with gtm

--- icpc_front/app/gallery/layout.jsx
+++ icpc_front/app/gallery/layout.jsx
@@ -46,19 +46,27 @@ export default function Layout({ children }) {
 
     return (
         <div className="w-full bg-white">
-            {/* Google Analytics */}
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-XQF50KFS2B"
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
+            
+           {/* Google Tag Manager */}
+            <Script id="google-tag-manager" strategy="afterInteractive">
                 {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-XQF50KFS2B');
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MGVBWT6D');
                 `}
             </Script>
+            
+            {/* Google Tag Manager (noscript) */}
+            <noscript>
+                <iframe 
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-MGVBWT6D"
+                    height="0" 
+                    width="0" 
+                    style={{ display: 'none', visibility: 'hidden' }}
+                />
+            </noscript>
             <Navbar open={open} hero={hero} darkSection={false} />
             <div className="max-w-screen bg-stone-300">
                 {children}
