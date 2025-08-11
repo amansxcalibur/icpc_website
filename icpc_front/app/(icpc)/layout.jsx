d2a9eb2256a0c2874865e0253f87324184a0ce91
AswinJ1 <aswinjhome@gmail.com>
2025-08-11 13:51:46 +0530

    test : chatbot uncomment

--- icpc_front/app/(icpc)/layout.jsx
+++ icpc_front/app/(icpc)/layout.jsx
@@ -107,7 +107,7 @@ export default function Layout({ children }) {
             <Navbar open={open} hero={hero} darkSection={darkSection} />
             <div className="max-w-screen md:-mt-[6vw] max-md:-mt-[-9vw] bg-stone-30 bg-white">
                 {children}
-                {/* <Terminal/> */}
+                <Terminal/>
             </div>
         </div>
     );
