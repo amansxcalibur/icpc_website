163f7d706a55eb76d7cb59b9b390bca2872345a4
Shreya B <shreyabaala2004@gmail.com>
2025-09-01 18:52:52 +0530

    Update layout.jsx

--- icpc_front/app/(icpc)/layout.jsx
+++ icpc_front/app/(icpc)/layout.jsx
@@ -109,7 +109,7 @@ export default function Layout({ children }) {
             <Script
                 id="noupe-chatbot"
                 strategy="afterInteractive"
-                src="https://www.noupe.com/embed/0198fb050ab07bf3a1521a18638011a8b539.js"
+                src="https://www.noupe.com/embed/019905659e8679fbb1ba96d551a1a44d4324.js"
             />
             
             <Navbar open={open} hero={hero} darkSection={darkSection} />
